4b696f69bd30ce201b3dc4f5eaae95763a781b90
tashton <ta@shpock.com>
2016-08-25 11:01:16 +0200

    constants

--- src/GeoHash.php
+++ src/GeoHash.php
@@ -3,6 +3,55 @@ namespace Lvht;
 
 class GeoHash
 {
+    /**
+     * A box accurate to 5,009.4km x 4,992.6km (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_LOW_KM_5009_X_4992 = 1;
+    /**
+     * A box accurate to 1,252.3km x 624.1km (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_LOW_KM_1252_X_624 = 2;
+    /**
+     * A box accurate to 156.5km x 156km (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_LOW_KM_156_X_156 = 3;
+    /**
+     * A box accurate to 39.1km x 19.5km (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_LOW_KM_39_X_19 = 4;
+    /**
+     * A box accurate to 4.9km x 4.9km (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_LOW_KM_5_X_5 = 5;
+    /**
+     * A box accurate to 1.2km x 609.4m (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_MEDIUM_M_1200_X_609 = 6;
+    /**
+     * A box accurate to 152.9m x 152.4m (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_MEDIUM_M_152_X_152 = 7;
+    /**
+     * A box accurate to 38.2m x 19m (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_MEDIUM_M_38_X_19 = 8;
+    /**
+     * A box accurate to 4.8m x 4.8m (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_MEDIUM_M_4_X_4 = 9;
+    /**
+     * A box accurate to 1.2m x 59.5cm (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_HIGH_CM_120_X_60 = 10;
+    /**
+     * A box accurate to 14.9cm x 14.9cm (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_HIGH_CM_14_X_14 = 11;
+    /**
+     * A box accurate to 3.7cm x 1.9cm (longitude x latitude)
+     */
+    const GEOHASH_PRECISION_HIGH_CM_3_X_1 = 12;
+
     private static $table = "0123456789bcdefghjkmnpqrstuvwxyz";
     private static $bits = array(
         0b10000, 0b01000, 0b00100, 0b00010, 0b00001
@@ -11,9 +60,9 @@ class GeoHash
     /**
      * Encode latitude and longitude to geohash
      *
-     * @param $lng
-     * @param $lat
-     * @param int $prec Precision of the geohash. Minimum 1, maximum 12.
+     * @param float $lng
+     * @param float $lat
+     * @param integer $prec Precision of the geohash. Minimum 1, maximum 12.
      * @return string
      */
     public static function encode($lng, $lat, $prec = 12)
